04dd3824d58b4b08ce8b62a09271e06fcf24cb69
thalissoncorreiadti <correia.tssc@gmail.com>
2019-05-22 15:20:09 -0300

    Nova Retro

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var slideIndex = 0;
 const slides = [
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=116497499&single=true',
+  'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista/',
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1986555468&single=true',
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1795008378&single=true',
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1042978375&single=true',
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSvu0yJzvhUaYgOo-DEbuog5DaOGiMuY0JtTC4UqsAZ0-IVdNPa8_ieAwdSUTkc1EKak8xGeDGYTzya/pubhtml?gid=1731783516&single=true',
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSGnRoRw6L1DdRMYjAvaTESfAMnUHa_MMlcS6kd-WanmZikSNnkBQw1CqPXQDg90-KvxVYyDW0sDf2l/pubhtml?gid=1227500108&single=true',
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vR-JPygtO61VSMXmqsxQ9WMUU2zpQ7rSx173wb2zLpWU4zUdOAXFnRoukH8zdGTZ2Xzg2M1zIs3eCu5/pubhtml?gid=1227500108&single=true'
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1042978375&single=true'
 ];
 
 let i = -1;
