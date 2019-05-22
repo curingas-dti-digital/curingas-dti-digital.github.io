49e90e7d7358c0f7a8ebb8de5856ee9bbc01eb49
thalissoncorreiadti <correia.tssc@gmail.com>
2019-05-22 18:22:13 -0300

    retro e eventos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var slideIndex = 0;
 const slides = [
-  'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista/',
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1986555468&single=true',
+  'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista/retro',
+  'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista/eventos',
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1795008378&single=true',
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1042978375&single=true'
 ];
