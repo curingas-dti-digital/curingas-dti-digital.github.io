c34a0c95644b9779494ac0956c32aef3a6849784
thalissoncorreiadti <correia.tssc@gmail.com>
2019-05-27 21:22:53 -0300

    Diagnóstico

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var slideIndex = 0;
 const slides = [
-  { page: 'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista', seconds: 60 },
+  { page: 'https://curingas-dti-digital.github.io/curingas-dti-gestao-a-vista', seconds: 90 },
   {
     page:
       'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1795008378&single=true',
-    seconds: 30
-  },
-  {
-    page:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNdMwLmKshoFx0i8LKIpuPhOiSEorQBMtUKt6LPWVRnCDWtJQCQ_WKxSQP_91ZZUdvHs635VDehn9H/pubhtml?gid=1042978375&single=true',
     seconds: 30
   }
 ];
